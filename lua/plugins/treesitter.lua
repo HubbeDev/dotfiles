fd52880160585997a0e2612db41f73dda72ab8b4
HubbeDev <privat@responsivhub.com>
2024-04-21 16:12:15 +0200

    add autopairs,wichkey, and update some config

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -4,7 +4,21 @@ return {
   config = function()
     local config = require("nvim-treesitter.configs")
     config.setup({
-      ensure_installed = {"lua", "javascript"},
+      ensure_installed = {
+        "lua",
+        "javascript",
+        "typescript",
+        "html",
+        "css",
+        "json",
+        "yaml",
+        "svelte",
+        "tsx",
+        "markdown",
+        "bash",
+        "vim",
+        "vimdoc"
+      },
       highlight = { enable = true },
       indent = { enable = true },
     })
