856fccfe2a04f7b86b2a9981ef24b4e31e994330
HubbeDev <privat@responsivhub.com>
2024-04-21 14:53:17 +0200

    Initial commit

---
+++ init.lua
@@ -0,0 +1,32 @@
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- vim options setup (set, g, wo, etc.)
+require("vim-options")
+
+
+-- Vim API setup (autocmds, commands, keymaps, etc.)
+require("vim-api")
+
+
+-- [[ Configure and install plugins ]]
+--
+--  To check the current status of your plugins, run
+--    :Lazy
+--
+--  You can press `?` in this menu for help. Use `:q` to close the window
+--
+--  To update plugins you can run
+--    :Lazy update
+--
+require("lazy").setup("plugins")
