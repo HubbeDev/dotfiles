4180da797efed3e31702d254456fa347ca09d71e
HubbeDev <privat@responsivhub.com>
2024-04-24 08:21:27 +0200

    nvim-biscuits

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,26 +1,26 @@
-return{
-  "nvim-neo-tree/neo-tree.nvim",
-  branch = "v3.x",
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-    "nvim-tree/nvim-web-devicons",
-    "MunifTanjim/nui.nvim",
-  },
-  config = function()
-    vim.keymap.set('n', '<C-b>', ':Neotree filesystem reveal left<CR>')
+return {
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+        "nvim-tree/nvim-web-devicons",
+        "MunifTanjim/nui.nvim",
+    },
+    config = function()
+        vim.keymap.set('n', '<C-b>', ':Neotree filesystem reveal left<CR>')
 
-    require("neo-tree").setup({
-      event_handlers = {
-        {
-            event = "file_opened",
-            handler = function(file_path)
-              -- auto close
-              -- vimc.cmd("Neotree close")
-              -- OR
-              require("neo-tree.command").execute({ action = "close" })
-            end
-        },
-      }
-    })
-  end
+        require("neo-tree").setup({
+            event_handlers = {
+                {
+                    event = "file_opened",
+                    handler = function(file_path)
+                        -- auto close
+                        -- vimc.cmd("Neotree close")
+                        -- OR
+                        require("neo-tree.command").execute({ action = "close" })
+                    end
+                },
+            }
+        })
+    end
 }
