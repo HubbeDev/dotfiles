e1950e559f792c220fd3ddc77e1a69c26dd91cd2
Hubbe <sebastian@fempunkter.se>
2024-04-29 16:37:37 +0200

    fix: linting php

--- nvim/lua/plugins/nvim-lint.lua
+++ nvim/lua/plugins/nvim-lint.lua
@@ -1,47 +1,50 @@
 return {
-	"mfussenegger/nvim-lint",
-	event = {
-		"BufWritePre",
-		"BufNewFile",
-	},
-	keys = {
-		{
-			"<leader>l",
-			function()
-				require("lint").try_lint()
-			end,
-			mode = "",
-			desc = "[Lint] current buffer",
-		},
-	},
-	config = function()
-		local lint = require("lint")
+    "mfussenegger/nvim-lint",
+    event = {
+        "BufWritePre",
+        "BufNewFile",
+    },
+    keys = {
+        {
+            "<leader>l",
+            function()
+                require("lint").try_lint()
+            end,
+            mode = "",
+            desc = "[Lint] current buffer",
+        },
+    },
+    config = function()
+        local lint = require("lint")
 
-		lint.linters_by_ft = {
-			javascript = { "eslint_d" },
-			typescript = { "eslint_d" },
-			svelte = { "eslint_d" },
-			json = { "jsonlint" },
-			php = { "phpcs" },
-			css = { "stylelint" },
-		}
+        lint.linters_by_ft = {
+            javascript = { "eslint_d" },
+            typescript = { "eslint_d" },
+            svelte = { "eslint_d" },
+            json = { "jsonlint" },
+            php = { "phpcs" },
+            css = { "stylelint" },
+        }
 
-		local phpcs = lint.linters.phpcs
-		phpcs.args = {
-			"--standard=WordPress",
-		}
+        local phpcs = lint.linters.phpcs
+        phpcs.args = {
+            '-q',
+            "--standard=PSR2",
+            '--report=json',
+            '-', -- need `-` at the end for stdin support
+        }
 
-		local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
+        local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
 
-		vim.api.nvim_create_autocmd(
-			{ "TextChanged", "TextChangedI", "BufEnter", "BufReadPre", "BufWritePost", "CursorHold", "CursorHoldI" },
-			{
-				group = lint_augroup,
-				callback = function()
-					lint.try_lint()
-				end,
-			}
-		)
-		vim.diagnostic.config({ virtual_text = true })
-	end,
+        vim.api.nvim_create_autocmd(
+            { "TextChanged", "TextChangedI", "BufEnter", "BufReadPre", "BufWritePost", "CursorHold", "CursorHoldI" },
+            {
+                group = lint_augroup,
+                callback = function()
+                    lint.try_lint()
+                end,
+            }
+        )
+        vim.diagnostic.config({ virtual_text = true })
+    end,
 }
