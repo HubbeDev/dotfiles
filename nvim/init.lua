70cc714646e57f67ee74125c67c32a0f8715b619
Hubbe <sebastian@fempunkter.se>
2024-08-15 16:33:02 +0200

    feat: kitty config base setup & some tmux changes

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,23 +1,21 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
 -- vim options setup (set, g, wo, etc.)
 require("vim-options")
 
-
 -- Vim API setup (autocmds, commands, keymaps, etc.)
 require("vim-api")
-
 
 -- [[ Configure and install plugins ]]
 --
