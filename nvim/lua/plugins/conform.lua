c1848fd02f7604404b2709b9e3759b1847a59cb0
hubbeandersson <sebastian@fempunkter.se>
2024-08-22 16:20:11 +0200

    wip: php formatting

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -22,7 +22,7 @@ return {
 	},
 	config = function()
 		local conform = require("conform")
-
+		local util = require("conform.util")
 		-- Function to detect WordPress coding standard
 		local function is_wordpress_standard()
 			local paths = { ".phpcs.xml", "phpcs.xml", ".phpcs.xml.dist", "phpcs.xml.dist" }
@@ -58,15 +58,18 @@ return {
 				yaml = { { "prettierd", "prettier" } },
 				markdown = { { "prettierd", "prettier" } },
 				php = function()
-					local cmd = get_local_bin("phpcbf")
-					if vim.fn.executable(cmd) == 1 then
-						return { "phpcbf" }
+					-- local cmd = get_local_bin("phpcbf")
+					if is_wordpress_standard() then
+						return { "phpcbf_wordpress" }
 					else
-						return { "php_cs_fixer" }
+						return { "phpcbf_psr12" }
 					end
 				end,
 			},
 			notify_on_error = true,
+			fallback_on_error = function(bufnr)
+				vim.notify("Formatting failed. Please check your code.", vim.log.levels.ERROR)
+			end,
 			format_on_save = function(bufnr)
 				local disable_filetypes = { c = true, cpp = true }
 				return {
@@ -76,14 +79,20 @@ return {
 				}
 			end,
 			formatters = {
-				phpcbf = {
-					command = get_local_bin("phpcbf"),
+				phpcbf_wordpress = {
+					meta = {
+						url = "https://phpqa.io/projects/phpcbf.html",
+						description = "PHP Code Beautifier and Fixer fixes violations of a defined coding standard.",
+					},
+					command = util.find_executable({
+						"vendor/bin/phpcbf",
+					}, "phpcbf"),
 					args = {
-						"--standard=WordPress", -- Adjust as necessary for your project,
 						"-q",
 						"--stdin-path=",
 						"$FILENAME",
 						"-",
+						"--standard=PSR12", -- Adjust as necessary for your project,
 					},
 					stdin = true,
 					-- 0: no errors found
@@ -93,6 +102,30 @@ return {
 					exit_codes = { 0, 1, 2 },
 					inherit = false,
 				},
+				phpcbf_psr12 = {
+					meta = {
+						url = "https://phpqa.io/projects/phpcbf.html",
+						description = "PHP Code Beautifier and Fixer fixes violations of a defined coding standard.",
+					},
+					command = util.find_executable({
+						"vendor/bin/phpcbf",
+					}, "phpcbf"),
+					args = {
+						"-q",
+						"--stdin-path=",
+						"$FILENAME",
+						"-",
+						"--standard=@PSR12", -- Adjust as necessary for your project,
+					},
+					stdin = true,
+					-- 0: no errors found
+					-- 1: errors found
+					-- 2: fixable errors found
+					-- 3: processing error
+					exit_codes = { 0, 1, 2 },
+					inherit = false,
+				},
+
 				php_cs_fixer = {
 					command = "php-cs-fixer",
 					args = {
@@ -103,6 +136,13 @@ return {
 					stdin = false,
 				},
 			},
+
+			-- Debugging: Track formatting steps
+			vim.api.nvim_create_autocmd("BufWritePost", {
+				callback = function()
+					vim.notify("File was formatted and saved successfully.", vim.log.levels.INFO)
+				end,
+			}),
 		})
 	end,
 }
